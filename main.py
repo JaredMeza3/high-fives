bef6e865008c659177bf3f624dbb57178f05336d
JaredMeza3 <jared.meza.students@gmail.com>
2025-03-27 13:07:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,4 @@ loops.every_interval(1000, on_every_interval)
 
 # tie it on the back of a hand then do a high five to measure the speed and acceleration of the high five
 # Inspired by (https://makecode.microbit.org/64433-01452-30313-87237)
+# To Access the data sheet go to file explorer and go to MY_DATA on the micro bit.
